aa60bc10f05e363621216060f68c0c46c9b8cf94
etherealtools <158293686+etherealtools@users.noreply.github.com>
2024-09-08 15:13:24 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -2,14 +2,14 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
+
 @app.route('/')
 def home():
-  return "Discord Tracker ok!"
+    return "Discord Tracker ok!"
 
 def ru():
-  app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
-  
+    t = Thread(target=ru)  # Corrected function call
+    t.start()
