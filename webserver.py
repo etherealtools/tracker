e6e29714a8b46dc95aa6757e8531ccab8776d8bb
etherealtools <158293686+etherealtools@users.noreply.github.com>
2024-09-08 15:08:01 +0200

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+@app.route('/')
+def home():
+  return "Discord Tracker ok!"
+
+def ru():
+  app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+  
